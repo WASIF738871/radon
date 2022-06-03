9453f90acaca4d536016b4feeb6917e4d1c37724
WASIF738871 <101876913+WASIF738871@users.noreply.github.com>
2022-06-03 14:09:53 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -44,13 +44,41 @@ router.get('/hello', function (req, res) {
    
 });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
+router.get('/sol1', function (req, res) {
+  
+   let arr= [1,2,3,5,6,7]
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let lastDigit= arr.pop()
+   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+   let missingNumber= consecutiveSum - total
+ 
+   res.send(  { data: missingNumber  }  );
+ });
+ 
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
+router.get('/sol2', function (req, res) {
+    let arr= [33, 34, 35, 37, 38]
+   let len= arr.length
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let firstDigit= arr[0]
+   let lastDigit= arr.pop()
+   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+   let missingNumber= consecutiveSum - total
+  
+   res.send(  { data: missingNumber  }  );
+ });
+
+  
 
 router.get('/test-me4', function (req, res) {
     res.send('My last api!')
