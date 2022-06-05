bd1715688e6afd116a284933dc1223c38334f782
WASIF738871 <101876913+WASIF738871@users.noreply.github.com>
2022-06-05 11:52:09 +0530

    platers assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -77,21 +77,45 @@ let players =
 
 
 router.post("/players", function(req, res) {
-    const newPlayer = req.body
+    // const newPlayer = req.body
     
-    for (let i = 0; i < players.length; i++) {
-        if (players[i].name == newPlayer["name"]){
-          console.log("player exists");
-          break;
+    // for (let i = 0; i < players.length; i++) {
+    //     if (players[i].name == newPlayer.name){
+    //       console.log("player exists");
+    //       break;
+    //     }
+    //     if (players[i].name !== newPlayer.name){
+    //         console.log("player is added");
+            
+    //       }
+    // }
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
         }
-        if (players[i].name !== newPlayer["name"]){
-            console.log("player is added");
-            players.push(newPlayer)
-          }
     }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
     
-    res.send({data:players,status:true})
-})
+    // res.send({data:players,status:true})
+    
+
 
 
 router.post("/test-post-2", function(req, res) {
